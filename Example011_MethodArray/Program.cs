cfefb192f7bbccae7b3b8858163c5bbbb7da7785
Artem <22ilden07@gmail.com>
2022-08-20 19:26:36 +0700

    Method Array

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 21, 31, 41, 18, 15, 16, 71, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while ( index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index +1;
+}

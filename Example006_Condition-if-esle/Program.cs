2accc5999c52ee17471563a3a1153040080b2a4f
Artem <22ilden07@gmail.com>
2022-08-20 12:58:49 +0700

    Condition-if-else

---
+++ Example006_Condition-if-esle/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура!!! Привет, Маша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
